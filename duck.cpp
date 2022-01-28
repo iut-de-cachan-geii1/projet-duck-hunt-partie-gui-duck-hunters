1152b22dd065e4e567f95615be56ecc875ff1ffa
valentin grateau <valentin.grateau@universite-paris-saclay.fr>
2022-01-28 15:00:52 +0100

    auto indentation et bonne écriture du else

--- duck.cpp
+++ duck.cpp
@@ -81,7 +81,7 @@ static qreal normalizeAngle(qreal angle)
 //! [0]
 Duck::Duck()
 {
-//    setTransform(QTransform().rotate(QRandomGenerator::global()->bounded(360 * 16)), true);
+    //    setTransform(QTransform().rotate(QRandomGenerator::global()->bounded(360 * 16)), true);
     startTimer(1000 / 33);
 }
 //! [0]
@@ -107,166 +107,163 @@ QPainterPath Duck::shape() const
 //! [3]
 void Duck::paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget)
 {
-    QLineF lineCenterToDestination(QPoint(0,0), mapFromScene(1201, 200));
+    QLineF lineCenterToDestination(QPoint(0, 0), mapFromScene(1201, 200));
 
     painter->drawLine(lineCenterToDestination);
 
     QPixmap imageDeCanard(":/images/canard.png");
-     painter->drawPixmap(QPoint(0,0),imageDeCanard);
+    painter->drawPixmap(QPoint(0, 0), imageDeCanard);
 
+    //    QMovie *movie = new QMovie(":/images/duck_animated.gif");
+    //    QLabel *processLabel = new QLabel(this);
+    //    processLabel->setMovie(movie);
+    //    movie->start();
 
+    // // Body
+    //    painter->setBrush(color);
+    //    painter->drawEllipse(-10, -20, 20, 40);
 
-//    QMovie *movie = new QMovie(":/images/duck_animated.gif");
-//    QLabel *processLabel = new QLabel(this);
-//    processLabel->setMovie(movie);
-//    movie->start();
+    //    // Eyes
+    //    painter->setBrush(Qt::white);
+    //    painter->drawEllipse(-10, -17, 8, 8);
+    //    painter->drawEllipse(2, -17, 8, 8);
 
+    //    // Nose
+    //    painter->setBrush(Qt::black);
+    //    painter->drawEllipse(QRectF(-2, -22, 4, 4));
 
-// // Body
-//    painter->setBrush(color);
-//    painter->drawEllipse(-10, -20, 20, 40);
+    //    // Pupils
+    //    painter->drawEllipse(QRectF(-8.0 + mouseEyeDirection, -17, 4, 4));
+    //    painter->drawEllipse(QRectF(4.0 + mouseEyeDirection, -17, 4, 4));
 
-//    // Eyes
-//    painter->setBrush(Qt::white);
-//    painter->drawEllipse(-10, -17, 8, 8);
-//    painter->drawEllipse(2, -17, 8, 8);
+    //    // Ears
+    //    painter->setBrush(scene()->collidingItems(this).isEmpty() ? Qt::darkYellow : Qt::red);
+    //    painter->drawEllipse(-17, -12, 16, 16);
+    //    painter->drawEllipse(1, -12, 16, 16);
 
-//    // Nose
-//    painter->setBrush(Qt::black);
-//    painter->drawEllipse(QRectF(-2, -22, 4, 4));
-
-//    // Pupils
-//    painter->drawEllipse(QRectF(-8.0 + mouseEyeDirection, -17, 4, 4));
-//    painter->drawEllipse(QRectF(4.0 + mouseEyeDirection, -17, 4, 4));
-
-//    // Ears
-//    painter->setBrush(scene()->collidingItems(this).isEmpty() ? Qt::darkYellow : Qt::red);
-//    painter->drawEllipse(-17, -12, 16, 16);
-//    painter->drawEllipse(1, -12, 16, 16);
-
-//    // Tail
-//    QPainterPath path(QPointF(0, 20));
-//    path.cubicTo(-5, 22, -5, 22, 0, 25);
-//    path.cubicTo(5, 27, 5, 32, 0, 30);
-//    path.cubicTo(-5, 32, -5, 42, 0, 35);
-//    painter->setBrush(Qt::NoBrush);
-//    painter->drawPath(path);
+    //    // Tail
+    //    QPainterPath path(QPointF(0, 20));
+    //    path.cubicTo(-5, 22, -5, 22, 0, 25);
+    //    path.cubicTo(5, 27, 5, 32, 0, 30);
+    //    path.cubicTo(-5, 32, -5, 42, 0, 35);
+    //    painter->setBrush(Qt::NoBrush);
+    //    painter->drawPath(path);
 }
 //! [3]
 void move(QWidget *widget)
 {
-
-
 }
 //! [4]
 void Duck::timerEvent(QTimerEvent *event)
 {
     QPointF pos_actuelle = pos();
-        QPointF position_target(1200,200);
+    QPointF position_target(1200, 200);
 
-        if((pos_actuelle.rx() != position_target.rx()))
+    if ((pos_actuelle.rx() != position_target.rx()))
+    {
+        if (pos_actuelle.ry() != position_target.ry())
         {
-            if(pos_actuelle.ry() != position_target.ry())
+            setPos(pos_actuelle - QPointF(-3, 3));
+        }
+        else
+        {
+            setPos(pos_actuelle + QPointF(3, 0));
+        }
+    }
+
+    //        else if (pos_actuelle.ry() != position_target.ry())
+    //        {
+
+    //            setPos(pos_actuelle - QPointF(0,1));
+    //        }
+
+    /*
+    //! [4]
+        // Don't move too far away
+    //! [5]
+        QLineF lineToCenter(QPointF(300, 40), mapFromScene(300, 300));
+        if (lineToCenter.length() > 150)
+        {
+            qreal angleToCenter = std::atan2(lineToCenter.dy(), lineToCenter.dx());
+            angleToCenter = normalizeAngle((Pi - angleToCenter) + Pi / 2);
+
+            if (angleToCenter < Pi && angleToCenter > Pi / 4)
             {
-                setPos(pos_actuelle - QPointF(-3,3));
+                // Rotate left
+                angle += (angle < -Pi / 2) ? 0.25 : -0.25;
             }
-            else setPos(pos_actuelle + QPointF(3,0));
+            else if (angleToCenter >= Pi && angleToCenter < (Pi + Pi / 2 + Pi / 4))
+            {
+                // Rotate right
+                angle += (angle < Pi / 2) ? 0.25 : -0.25;
+            }
         }
-
-//        else if (pos_actuelle.ry() != position_target.ry())
-//        {
-
-//            setPos(pos_actuelle - QPointF(0,1));
-//        }
-
-/*
-//! [4]
-    // Don't move too far away
-//! [5]
-    QLineF lineToCenter(QPointF(300, 40), mapFromScene(300, 300));
-    if (lineToCenter.length() > 150)
-    {
-        qreal angleToCenter = std::atan2(lineToCenter.dy(), lineToCenter.dx());
-        angleToCenter = normalizeAngle((Pi - angleToCenter) + Pi / 2);
-
-        if (angleToCenter < Pi && angleToCenter > Pi / 4)
+        else if (::sin(angle) < 0)
         {
-            // Rotate left
-            angle += (angle < -Pi / 2) ? 0.25 : -0.25;
+            angle += 0.25;
         }
-        else if (angleToCenter >= Pi && angleToCenter < (Pi + Pi / 2 + Pi / 4))
+        else if (::sin(angle) > 0)
         {
-            // Rotate right
-            angle += (angle < Pi / 2) ? 0.25 : -0.25;
+            angle -= 0.25;
+    //! [5] //! [6]
         }
-    }
-    else if (::sin(angle) < 0)
-    {
-        angle += 0.25;
-    }
-    else if (::sin(angle) > 0)
-    {
-        angle -= 0.25;
-//! [5] //! [6]
-    }
-//! [6]
+    //! [6]
 
-//    // Try not to crash with any other mice
-////! [7]
-//    QList<QGraphicsItem *> dangerMice = scene()->items(QPolygonF()
-//                                                       << mapToScene(0, 0)
-//                                                       << mapToScene(-30, -50)
-//                                                       << mapToScene(30, -50));
-//    for (QGraphicsItem *item : dangerMice)
-//    {
-//        if (item == this)
-//        {
-//            continue;
-//        }
+    //    // Try not to crash with any other mice
+    ////! [7]
+    //    QList<QGraphicsItem *> dangerMice = scene()->items(QPolygonF()
+    //                                                       << mapToScene(0, 0)
+    //                                                       << mapToScene(-30, -50)
+    //                                                       << mapToScene(30, -50));
+    //    for (QGraphicsItem *item : dangerMice)
+    //    {
+    //        if (item == this)
+    //        {
+    //            continue;
+    //        }
 
-//        QLineF lineToMouse(QPointF(0, 0), mapFromItem(item, 0, 0));
-//        qreal angleToMouse = std::atan2(lineToMouse.dy(), lineToMouse.dx());
-//        angleToMouse = normalizeAngle((Pi - angleToMouse) + Pi / 2);
+    //        QLineF lineToMouse(QPointF(0, 0), mapFromItem(item, 0, 0));
+    //        qreal angleToMouse = std::atan2(lineToMouse.dy(), lineToMouse.dx());
+    //        angleToMouse = normalizeAngle((Pi - angleToMouse) + Pi / 2);
 
-//        if (angleToMouse >= 0 && angleToMouse < Pi / 2)
-//        {
-//            // Rotate right
-//            angle += 0.5;
-//        }
-//        else if (angleToMouse <= TwoPi && angleToMouse > (TwoPi - Pi / 2))
-//        {
-//            // Rotate left
-//            angle -= 0.5;
-////! [7] //! [8]
-//        }
-////! [8] //! [9]
-//    }
-////! [9]
+    //        if (angleToMouse >= 0 && angleToMouse < Pi / 2)
+    //        {
+    //            // Rotate right
+    //            angle += 0.5;
+    //        }
+    //        else if (angleToMouse <= TwoPi && angleToMouse > (TwoPi - Pi / 2))
+    //        {
+    //            // Rotate left
+    //            angle -= 0.5;
+    ////! [7] //! [8]
+    //        }
+    ////! [8] //! [9]
+    //    }
+    ////! [9]
 
-//    // Add some random movement
-////! [10]
-//    if (dangerMice.size() > 1 && QRandomGenerator::global()->bounded(10) == 0)
-//    {
-//        if (QRandomGenerator::global()->bounded(1) != 0)
-//        {
-//            angle += QRandomGenerator::global()->bounded(1 / 500.0);
+    //    // Add some random movement
+    ////! [10]
+    //    if (dangerMice.size() > 1 && QRandomGenerator::global()->bounded(10) == 0)
+    //    {
+    //        if (QRandomGenerator::global()->bounded(1) != 0)
+    //        {
+    //            angle += QRandomGenerator::global()->bounded(1 / 500.0);
 
-//        }
-//        else
-//        {
-//            angle -= QRandomGenerator::global()->bounded(1 / 500.0);
-//        }
-//    }
-////! [10]
+    //        }
+    //        else
+    //        {
+    //            angle -= QRandomGenerator::global()->bounded(1 / 500.0);
+    //        }
+    //    }
+    ////! [10]
 
-//! [11]
-    speed += (-50 + QRandomGenerator::global()->bounded(1000000)) / 100.0;
+    //! [11]
+        speed += (-50 + QRandomGenerator::global()->bounded(1000000)) / 100.0;
 
-    qreal dx = ::sin(angle) * 10;
-    mouseEyeDirection = (qAbs(dx / 5) < 1) ? 0 : dx / 5;
+        qreal dx = ::sin(angle) * 10;
+        mouseEyeDirection = (qAbs(dx / 5) < 1) ? 0 : dx / 5;
 
-    setTransform(QTransform().rotate(dx), true);
-    setPos(mapToParent(0, -(3 + sin(speed) * 3)));*/
-
+        setTransform(QTransform().rotate(dx), true);
+        setPos(mapToParent(0, -(3 + sin(speed) * 3)));*/
 }
 //! [11]
