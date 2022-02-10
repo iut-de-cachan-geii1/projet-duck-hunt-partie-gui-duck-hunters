b5647fec6dd3633d673c74f78be9e30bf46ea238
llefessantge1 <luc.le-fessant@universite-paris-saclay.fr>
2022-02-10 09:17:18 +0100

    commit erreurs merge

--- duck.cpp
+++ duck.cpp
@@ -96,15 +96,15 @@ Duck::Duck()
 
 QRectF Duck::boundingRect() const
 {
-    // qreal adjust = 0.5;
+    qreal adjust = 0.5;
     return QRectF(-18 - adjust, -22 - adjust,
                   36 + adjust, 60 + adjust);
 }
 
 QPainterPath Duck::shape() const
 {
-    // QPainterPath path;
-    // path.addRect(0, 0, 75, 71);
+    QPainterPath path;
+    path.addRect(0, 0, 75, 71);
     return path;
 }
 
